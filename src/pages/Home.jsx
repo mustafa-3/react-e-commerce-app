076a2b79c272f5f521aa929d3d0207be7fceee21
mustafa-3 <sagunmustafa14@gmail.com>
2022-10-29 14:12:11 +0300

    v2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,12 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <div>Home</div>
+      <div className="h-screen  ">
+        <div className="text font-bold underline sm:text-center md:text-right">
+          Home
+        </div>
+        <button className="btn btn-outline btn-lg btn-ghost">Button</button>
+      </div>
     </>
   );
 };
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import Products from "../pages/Products";
 import Register from "../pages/Register";
 
 const AppRouter = () => {
@@ -11,6 +12,7 @@ const AppRouter = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </BrowserRouter>
   );
