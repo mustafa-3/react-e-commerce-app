001f7013e5ca5805c96b5945ff94757dfa8d7796
mustafa-3 <sagunmustafa14@gmail.com>
2022-11-01 12:42:21 +0300

    v3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,12 @@
 import React from "react";
+import Carousel from "../components/Carousel";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <div className="h-screen  ">
-        <div className="text font-bold underline sm:text-center md:text-right">
-          Home
-        </div>
-        <button className="btn btn-outline btn-lg btn-ghost">Button</button>
-      </div>
+      <Carousel />
     </>
   );
 };
